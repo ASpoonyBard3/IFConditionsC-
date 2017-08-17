a4a7a20c79101796c1619d7f1d6e50dbef6139c0
Gatlingover <Samsonf21@gmail.com>
2017-08-17 15:41:21 +0100

    Really broken c# application

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,21 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
+using System;
 
-#include "stdafx.h"
-
-
-int main()
+namespace FirstConsoleProject
 {
-    return 0;
+	Class MainClass
+	{
+		Public static void Main (String[] args) // this is a method called "Main". It is called when the program starts.
+		{
+			Console.WriteLine {"How old are you?"};
+			int age = Convert.ToInt32(Console.ReadLine());
+			if (age < 18) {
+				Console.WriteLine("That's too bad! You will have to wait a couple of years.");
+			} else if (age == 18) {
+				Console.WriteLine("Puh, barely made it!");
+			} else {
+				Console.WriteLine("You are not allowed to enter.");
+			}
+			Console.ReadKey();
+		}
+	}
 }
-
